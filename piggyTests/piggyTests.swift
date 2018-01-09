b84f6b9e07ed7acc69f677cc6e387045ba91d958
hawk <hawk@hawkde-MacBook-Pro.local>
2018-01-09 17:20:29 +0800

    piggy

---
+++ piggyTests/piggyTests.swift
@@ -0,0 +1,36 @@
+//
+//  piggyTests.swift
+//  piggyTests
+//
+//  Created by hawk on 2018/1/5.
+//  Copyright © 2018年 Test. All rights reserved.
+//
+
+import XCTest
+@testable import piggy
+
+class piggyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
